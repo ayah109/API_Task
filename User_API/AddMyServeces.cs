ef93bcc56c3cbd9746aadec6aec3c387583b5128
Aya Hussain <ayatalep816@gmail.com>
2022-07-26 14:05:23 +0300

    ExtentionMethod

---
+++ User_API/AddMyServeces.cs
@@ -0,0 +1,22 @@
+﻿using User_API.Model;
+
+namespace User_API
+{
+    public static class AddMyServeces
+    {
+        public static void InitServ(this IServiceCollection iserv)
+        {
+            iserv.AddEndpointsApiExplorer();
+
+            iserv.AddSwaggerGen();
+
+            iserv.AddControllers();
+
+            iserv.AddScoped<User_API.Repo.IUser_Repo, User_API.Repo.User_Repo>();
+
+            iserv.AddScoped<User_API.Repo.IPost_Repo, User_API.Repo.Post_Repo>();
+
+        }
+
+    }
+}
--- User_API/Program.cs
+++ User_API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using User_API.Model;
 using User_API.Repo;
+using User_API;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,14 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+
 
 builder.Services.AddDbContext<UserContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
-builder.Services.AddScoped<User_API.Repo.IUser_Repo, User_API.Repo.User_Repo>();
+builder.Services.InitServ();
+//builder.Services.AddScoped<User_API.Repo.IUser_Repo, User_API.Repo.User_Repo>();
 
-builder.Services.AddScoped<User_API.Repo.IPost_Repo, User_API.Repo.Post_Repo>();
+//builder.Services.AddScoped<User_API.Repo.IPost_Repo, User_API.Repo.Post_Repo>();
 
 
 var app = builder.Build();
